0031db4c58284f1aab9b613dbd6de73404cac6b9
SkySom <skylar.som@gmail.com>
2018-08-18 04:21:04 -0400

    Add Wand and Powders

--- src/main/java/com/minerarcana/transfiguration/Transfiguration.java
+++ src/main/java/com/minerarcana/transfiguration/Transfiguration.java
@@ -1,21 +1,35 @@
 package com.minerarcana.transfiguration;
 
+import com.google.common.collect.Lists;
+import com.minerarcana.transfiguration.item.ItemMagicPowder;
+import com.minerarcana.transfiguration.item.ItemWand;
 import com.teamacronymcoders.base.BaseModFoundation;
+import com.teamacronymcoders.base.event.BaseRegistryEvent;
+import com.teamacronymcoders.base.recipesystem.RecipeType;
+import com.teamacronymcoders.base.registrysystem.ItemRegistry;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.List;
+
+@EventBusSubscriber
 @Mod(modid = Transfiguration.ID, name = Transfiguration.NAME, version = Transfiguration.VERSION)
 public class Transfiguration extends BaseModFoundation<Transfiguration> {
     public static final String ID = "transfiguration";
     public static final String NAME = "Transfiguration";
     public static final String VERSION = "@VERSION@";
 
+    public static final List<RecipeType> recipeTypes = Lists.newArrayList();
+
     public Transfiguration() {
-        super(ID, NAME, VERSION, CreativeTabs.FOOD);
+        super(ID, NAME, VERSION, CreativeTabs.MISC);
     }
 
     @EventHandler
@@ -37,6 +51,19 @@ public class Transfiguration extends BaseModFoundation<Transfiguration> {
     }
 
     @Override
+    public void registerItems(ItemRegistry registry) {
+        super.registerItems(registry);
+        registry.register(new ItemWand());
+
+        String[] powders = new String[]{"accursed", "lucky"};
+        for (String powder : powders) {
+            ItemMagicPowder itemMagicPowder = new ItemMagicPowder(powder);
+            registry.register(itemMagicPowder);
+            recipeTypes.add(itemMagicPowder.getRecipeType());
+        }
+    }
+
+    @Override
     public boolean hasExternalResources() {
         return true;
     }
@@ -44,5 +71,13 @@ public class Transfiguration extends BaseModFoundation<Transfiguration> {
     @Override
     public Transfiguration getInstance() {
         return this;
+    }
+
+    @SubscribeEvent
+    public static void registerTypes(BaseRegistryEvent<RecipeType> registryEvent) {
+        registryEvent.register(new ResourceLocation(ID, "wand"), new RecipeType(new ResourceLocation(ID, "wand")));
+        for (RecipeType recipeType : recipeTypes) {
+            registryEvent.register(new ResourceLocation(recipeType.name), recipeType);
+        }
     }
 }
---
+++ src/main/java/com/minerarcana/transfiguration/item/ItemWand.java
@@ -0,0 +1,9 @@
+package com.minerarcana.transfiguration.item;
+
+import com.teamacronymcoders.base.items.ItemBase;
+
+public class ItemWand extends ItemBase {
+    public ItemWand() {
+        super("wand");
+    }
+}
